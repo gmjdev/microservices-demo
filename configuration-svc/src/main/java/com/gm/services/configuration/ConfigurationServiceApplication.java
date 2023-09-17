62e283401faf82b2c14be3b0b00408dd43e93e8c
Girish Mahajan <gm.jdev@gmail.com>
2023-09-18 00:07:13 +0530

    skaffold integration

--- configuration-svc/src/main/java/com/gm/services/configuration/ConfigurationServiceApplication.java
+++ configuration-svc/src/main/java/com/gm/services/configuration/ConfigurationServiceApplication.java
@@ -1,13 +1,16 @@
 package com.gm.services.configuration;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+@Slf4j
 public class ConfigurationServiceApplication {
 	public static void main(String[] args) {
+		log.debug("Hello");
 		SpringApplication.run(ConfigurationServiceApplication.class, args);
 	}
 }
